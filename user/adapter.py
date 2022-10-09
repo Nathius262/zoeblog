e41e0d3c5fdd5b1847e6facda02352ceab0bb5d5
Nathaniel Egbodo <egbodonathaniel@yahoo.com>
2022-10-09 22:38:16 +0100

    #

--- user/adapter.py
+++ user/adapter.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.account import app_settings
 from allauth.utils import import_attribute
@@ -31,3 +32,38 @@ def pre_save_pre_social_login_receiver(sender, instance, sociallogin, *args, **k
 
 
 pre_social_login.connect(pre_save_pre_social_login_receiver, sender=Account)
+=======
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account import app_settings
+from allauth.utils import import_attribute
+from allauth.account.signals import user_signed_up
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.signals import pre_social_login
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from profileAccount.models import Account
+from django.conf import settings
+
+
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def get_email_confirmation_url(self, request, emailconfirmation):
+        url = "https://zoeblog.pythonanywhere/account/confirm-email/" + emailconfirmation.key
+        return url
+
+def get_adapter(request=None):
+    """
+    The Adapter in app_settings.ADAPTER is set to CustomAccountAdapter.
+    """
+    return import_attribute(app_settings.ADAPTER)(request)
+
+
+def pre_save_pre_social_login_receiver(sender, instance, sociallogin, *args, **kwargs):
+    instance.picture = sociallogin.account.extra_data.get('picture', None)
+    instance.name = 'yes'
+    instance.save()
+
+
+pre_social_login.connect(pre_save_pre_social_login_receiver, sender=Account)
+>>>>>>> main
