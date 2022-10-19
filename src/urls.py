46337b9c407418a58b987c5dfecd663c19ebe69c
Nathaniel Egbodo <egbodonathaniel@yahoo.com>
2022-10-19 12:00:54 +0100

    #

--- src/urls.py
+++ src/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('search/', search_view, name='search'),
     path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),  #add the robots.txt file
-    path("dns.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),  #add the robots.txt file
+    path("dns.txt",TemplateView.as_view(template_name="dns.txt", content_type="text/plain")),  #add the robots.txt file
     path('googleb1215a559aaa2a13.html', GoogleVerifyView.as_view()),
 
     path('profile/', include('user.urls', 'user')),
